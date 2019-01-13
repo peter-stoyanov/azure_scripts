0f25e2f1c3a966065e78b41512df2d747fecdba0
Petar Stoyanov <pesho.stoyanov@gmail.com>
2019-01-13 11:02:57 +0200

    Create vm_setup.ps1

---
+++ vm_setup.ps1
@@ -0,0 +1,110 @@
+New-AzureRmResourceGroup -ResourceGroupName myRGNetwork -Location EastUS
+
+$frontendSubnet = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name myFrontendSubnet `
+  -AddressPrefix 10.0.0.0/24
+ 
+ $backendSubnet = New-AzureRmVirtualNetworkSubnetConfig `
+  -Name myBackendSubnet `
+  -AddressPrefix 10.0.1.0/24
+ 
+ $vnet = New-AzureRmVirtualNetwork `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -Name myVNet `
+  -AddressPrefix 10.0.0.0/16 `
+  -Subnet $frontendSubnet, $backendSubnet
+ 
+ $pip = New-AzureRmPublicIpAddress `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -AllocationMethod Dynamic `
+  -Name myPublicIPAddress
+ 
+ $frontendNic = New-AzureRmNetworkInterface `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -Name myFrontend `
+  -SubnetId $vnet.Subnets[0].Id `
+  -PublicIpAddressId $pip.Id
+  
+  $cred = Get-Credential
+  
+  New-AzureRmVM `
+   -Credential $cred `
+   -Name myFrontend `
+   -PublicIpAddressName myPublicIPAddress `
+   -ResourceGroupName myRGNetwork `
+   -Location "EastUS" `
+   -Size Standard_D1 `
+   -SubnetName myFrontendSubnet `
+   -VirtualNetworkName myVNet
+   
+  $nsgFrontendRule = New-AzureRmNetworkSecurityRuleConfig `
+  -Name myFrontendNSGRule `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 200 `
+  -SourceAddressPrefix * `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 80 `
+  -Access Allow
+  
+  $nsgBackendRule = New-AzureRmNetworkSecurityRuleConfig `
+  -Name myBackendNSGRule `
+  -Protocol Tcp `
+  -Direction Inbound `
+  -Priority 100 `
+  -SourceAddressPrefix 10.0.0.0/24 `
+  -SourcePortRange * `
+  -DestinationAddressPrefix * `
+  -DestinationPortRange 1433 `
+  -Access Allow
+  
+ $nsgFrontend = New-AzureRmNetworkSecurityGroup `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -Name myFrontendNSG `
+  -SecurityRules $nsgFrontendRule
+  
+ $nsgBackend = New-AzureRmNetworkSecurityGroup `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -Name myBackendNSG `
+  -SecurityRules $nsgBackendRule
+  
+  $vnet = Get-AzureRmVirtualNetwork `
+  -ResourceGroupName myRGNetwork `
+  -Name myVNet
+$frontendSubnet = $vnet.Subnets[0]
+$backendSubnet = $vnet.Subnets[1]
+$frontendSubnetConfig = Set-AzureRmVirtualNetworkSubnetConfig `
+  -VirtualNetwork $vnet `
+  -Name myFrontendSubnet `
+  -AddressPrefix $frontendSubnet.AddressPrefix `
+  -NetworkSecurityGroup $nsgFrontend
+$backendSubnetConfig = Set-AzureRmVirtualNetworkSubnetConfig `
+  -VirtualNetwork $vnet `
+  -Name myBackendSubnet `
+  -AddressPrefix $backendSubnet.AddressPrefix `
+  -NetworkSecurityGroup $nsgBackend
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+
+$backendNic = New-AzureRmNetworkInterface `
+  -ResourceGroupName myRGNetwork `
+  -Location EastUS `
+  -Name myBackend `
+  -SubnetId $vnet.Subnets[1].Id
+  
+ New-AzureRmVM `
+   -Credential $cred `
+   -Name myBackend `
+   -ImageName "MicrosoftSQLServer:SQL2016SP1-WS2016:Enterprise:latest" `
+   -ResourceGroupName myRGNetwork `
+   -Location "EastUS" `
+   -SubnetName MyBackendSubnet `
+   -VirtualNetworkName myVNet
+  
+  
+ 
